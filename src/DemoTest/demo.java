84d6ef93dc3d7fd4db45853e361a1cf40d793eed
rshvashisht <rvashisht86@gmail.com>
2024-01-23 16:32:11 +0530

    First Commit

---
+++ src/DemoTest/demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package DemoTest;
+
+/**
+ * 
+ */
+public class demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
+
+}
